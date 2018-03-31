d19309e0e2fc38ba05012cb8a024b99b66ec4908
jpdattas <jpdattas@uc.cl>
2018-03-31 18:11:57 -0300

    seed arreglada

--- db/migrate/20180319104932_devise_create_users.rb
+++ db/migrate/20180319104932_devise_create_users.rb
@@ -6,8 +6,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      ## Admin
-      t.boolean :admin
 
       ## Recoverable
       t.string   :reset_password_token
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.new
+user.email = 'admin@admin.admin'
+user.password = 'administrador'
+user.admin = true
+user.save!
 
-users = User.create([{ email: 'admin@admin.admin' admin:True }
+# def create_user(mail,pass,admin,photo)
+#   user = User.find_or_create_by(email: mail)
+#   user.password = pass
+#   user.admin = admin
+#   user.photo=photo
+#   user.save
+# end
+#
+#
+#
+# def create(media,type)
+#   case type
+#   when "users"
+#     for aux in media
+#       create_user(aux[0],aux[1],aux[2],aux[3])
+#     end
+#   end
+# end
+
+
+#User.create([{ email: 'admin@admin.admin' admin:True }
